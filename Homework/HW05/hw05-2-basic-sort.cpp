b28547b0a74a40d1357ad5b9b77fabf00c1f523c
Jirphat Saetae <jirphat.aok@gmail.com>
2023-08-09 22:48:25 +0700

    HW502

--- Homework/HW05/hw05-2-basic-sort.cpp
+++ Homework/HW05/hw05-2-basic-sort.cpp
@@ -21,3 +21,30 @@
     Output:
         112 91 65
 */
+#include <stdio.h>
+int main() {
+    int Num1,Num2,Num3 ;
+    printf( "Input[1] : ") ;
+    scanf( "%d",&Num1) ;
+    printf( "Input[2] : " ) ;
+    scanf( "%d",&Num2) ;
+    printf( "Input[3] : " ) ;
+    scanf( "%d",&Num3) ;
+	if( Num1 < Num2 && Num2 < Num3 ){
+		printf( "%d %d %d ",Num1,Num2,Num3) ;
+		}else if( Num1 < Num3 && Num3 < Num2 ) {
+			printf( "%d %d %d ",Num1,Num3,Num2) ;
+		}else if( Num2 < Num1 && Num1 < Num3 ) {
+			printf( "%d %d %d ",Num2,Num1,Num3) ;
+		}else if( Num2 < Num3 && Num3 < Num1 ) {
+			printf( "%d %d %d ",Num2,Num3,Num1) ;
+		}else if( Num3 < Num2 && Num2 < Num1 ) {
+			printf( "%d %d %d ",Num3,Num2,Num1) ;
+		}else if( Num3 < Num1 && Num1 < Num2 ) {
+			printf( "%d %d %d ",Num3,Num1,Num2) ;
+		}
+		else {
+			printf( "please enter number only." ) ;
+			}//end if
+	return 0 ;
+	}
